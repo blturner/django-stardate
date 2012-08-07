b7b475706c661f8e23c0fe33364e81fe3929803b
Benjamin Turner <benturn@gmail.com>
2012-08-07 15:27:13 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='stardate',
+    version='0.1',
+    author=u'Benjamin Turner',
+    author_email='benturn@gmail.com',
+    packages=['stardate'],
+    # url='https://github.com/blturner/stitch',
+    # license='BSD licence, see LICENCE.txt',
+    # description='A tool for managing django settings.',
+    # long_description=open('README').read(),
+    zip_safe=False,
+)
