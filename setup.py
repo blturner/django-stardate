b02057f6ca00b0098d38402578b4644661ce7e26
Benjamin Turner <benturn@gmail.com>
2012-08-12 01:38:01 -0700

    try again

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1',
     author=u'Benjamin Turner',
     author_email='benturn@gmail.com',
-    packages=['stardate', 'urls'],
+    packages=['stardate', 'stardate.urls'],
     # url='https://github.com/blturner/stitch',
     # license='BSD licence, see LICENCE.txt',
     # description='A tool for managing django settings.',
