3636b3ce327aed6deb15ca64383279a7ae17f2f4
Benjamin Turner <benturn@gmail.com>
2012-11-12 16:08:43 -0800

    added rss

---
+++ stardate/feeds.py
@@ -0,0 +1,31 @@
+from django.contrib.syndication.views import Feed
+from django.shortcuts import get_object_or_404
+from markdown import markdown
+
+from stardate.models import Blog, Post
+
+
+class LatestPostsFeed(Feed):
+    def get_object(self, request, blog_slug):
+        return get_object_or_404(Blog, slug=blog_slug)
+
+    def title(self, obj):
+        return "%s: Recent posts" % obj.name
+
+    def link(self, obj):
+        return obj.get_absolute_url()
+
+    def items(self, obj):
+        return Post.objects.published().filter(blog=obj)[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_link(self, item):
+        return item.get_absolute_url()
+
+    def item_pubdate(self, item):
+        return item.publish
+
+    def item_description(self, item):
+        return markdown(item.body)[:300]
--- stardate/urls/blog_urls.py
+++ stardate/urls/blog_urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
 
+from stardate.feeds import LatestPostsFeed
 from stardate import views
 
 
 urlpatterns = patterns('',
     url(r'^$', views.PostArchiveIndex.as_view(), name='post-archive-index'),
+    url(r'rss/$', LatestPostsFeed(), name='post-feed'),
     url(r'^(?P<year>\d{4})/$', views.PostYearArchive.as_view(), name='post-archive-year'),
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$', views.PostMonthArchive.as_view(), name='post-archive-month'),
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/$', views.PostDayArchive.as_view(), name='post-archive-day'),
