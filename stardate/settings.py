5a7f8e573cf46aa42c877ddb5e0198f4329206d3
Benjamin Turner <benturn@gmail.com>
2013-03-10 15:14:50 -0700

    removed tokens setting

--- stardate/settings.py
+++ stardate/settings.py
@@ -1,5 +1,3 @@
-import os
-
 from django.conf import settings
 
 PROJECT_ROOT = getattr(settings, 'PROJECT_ROOT', None)
@@ -7,6 +5,3 @@ PROJECT_ROOT = getattr(settings, 'PROJECT_ROOT', None)
 DROPBOX_APP_KEY = getattr(settings, 'DROPBOX_APP_KEY', None)
 DROPBOX_APP_SECRET = getattr(settings, 'DROPBOX_APP_SECRET', None)
 DROPBOX_ACCESS_TYPE = 'app_folder'
-
-TOKENS_FILEPATH = getattr(settings, 'TOKENS_FILEPATH',
-    os.path.join(PROJECT_ROOT, 'dropbox_tokens'))
