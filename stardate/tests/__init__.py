21039162a6cc4fa7aa428847d4d131e1fc651bbc
Benjamin Turner <benturn@gmail.com>
2013-04-03 19:32:17 -0700

    renamed tests

--- stardate/tests/__init__.py
+++ stardate/tests/__init__.py
@@ -1,6 +1,4 @@
-# from stardate.tests.models import *
-# from stardate.tests.views import *
-# from stardate.tests.parser import *
-# from stardate.tests.sync import *
+from stardate.tests.test_models import *
+from stardate.tests.test_views import *
 from stardate.tests.test_backends import *
 from stardate.tests.test_parsers import *
