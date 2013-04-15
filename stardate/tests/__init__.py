f70e2b4b0955d9ad75fb0ea503699ebe3877576c
Benjamin Turner <benturn@gmail.com>
2013-04-14 22:19:54 -0700

    removed unnecessary test file

--- stardate/tests/__init__.py
+++ stardate/tests/__init__.py
@@ -1,4 +1,3 @@
 from stardate.tests.test_models import *
-from stardate.tests.test_views import *
 from stardate.tests.test_backends import *
 from stardate.tests.test_parsers import *
--- stardate/tests/test_views.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-
-class StardateViewsTestCase(TestCase):
-    def test_fake(self):
-        self.assertEqual(1 + 1, 2)
