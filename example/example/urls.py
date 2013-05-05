68f251424d363e881222667947cb1c55de36543d
Benjamin Turner <benturn@gmail.com>
2013-05-04 22:47:23 -0700

    tweak example urls

--- example/example/urls.py
+++ example/example/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
-	url(r'', include('social_auth.urls')),
-    url(r'blog', include('stardate.urls.index_urls')),
+    url(r'', include('social_auth.urls')),
+    url(r'^blog/', include('stardate.urls.index_urls')),
 )
