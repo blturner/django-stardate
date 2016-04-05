68bc9ee2f69f99d062224192469bc4c90e64db4a
Benjamin Turner <benturn@gmail.com>
2016-04-05 10:15:13 -0400

    remove test

--- stardate/backends/local_file.py
+++ stardate/backends/local_file.py
@@ -62,15 +62,6 @@ class LocalFileBackend(StardateBackend):
     def _list_path(self, path):
         return os.listdir(path)
 
-    # def _get_post_path(self, folder, post):
-    #     """
-    #     Dynamically guess post file path from slug / blog folder
-    #     """
-    #     filename = post['slug']
-    #     filename = '{0}.md'.format(filename)
-    #     path = os.path.join(folder, filename)
-    #     return path
-
     @property
     def last_sync(self):
         modified = time.ctime(os.path.getmtime(self.blog.backend_file))
