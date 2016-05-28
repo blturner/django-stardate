e127311cf1df169efffff6e5fe9342b2ad4894e4
Benjamin Turner <benturn@gmail.com>
2016-05-28 17:05:32 -0400

    get the blog

--- stardate/views.py
+++ stardate/views.py
@@ -210,7 +210,9 @@ def process_user(user):
     logger.debug('processing: {}'.format(user))
     try:
         social_auth = UserSocialAuth.objects.get(uid=user)
-        blogs = social_auth.user.blog_set.all()
+        blogs = social_auth.blog_set.all()
+
+        logger.debug('found {} blog(s)'.format(len(blogs)))
 
         for blog in blogs:
             logger.info('processing {}'.format(blog.name))
